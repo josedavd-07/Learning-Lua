fa922d4a3f73852d4e317824914f3099165c45ab
JOSE DAVID CARRANZA ANGARITA <ing.josedavidcarranzaangarita@gmail.com>
2025-03-07 11:22:59 -0500

    Aca añadimos datos interesantes sobre el lenguaje lua

---
+++ InformationGame/search.lua
@@ -0,0 +1,6 @@
+--Cuando usar las variables globales y locales en Lua para videojuegos
+
+--[[
+    Globales → Datos compartidos como salud del jugador, puntuación o nivel actual.
+    Locales → Datos temporales como coordenadas de enemigos, daño momentáneo o cálculos
+]]--
