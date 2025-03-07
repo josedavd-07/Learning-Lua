7492a7de4d3938dc2026dbfdaa408dea2f55aa35
JOSE DAVID CARRANZA ANGARITA <ing.josedavidcarranzaangarita@gmail.com>
2025-03-06 23:16:00 -0500

    Se reorganizo los archivos  solo quedo el hola mundo

--- 01_holamundo.lua
+++ 01_holamundo.lua
@@ -2,29 +2,3 @@
 
 print("Hello World, este es el primer hola mundo deel lenguaje lua credao el dia  6 de marzo del 2025")
 
---***********SECCION DE TIPOS DE DATOS*************
-
---TIPOS DE DATOS DE TEXTO
---STRING
---USAMOS CONCATENACION PARA UNIR DOS STRINGS
-local name = "Juan"
-local lastname = "Perez"
-local fullname = name .. " " .. lastname
-print(fullname)
-
---Char
-local char = "a"
-print(char)
-
---TIPOS DE DATOS NUMERICOS
---INT
-local age = 25
-print(age)
-
---FLOAT
-local height = 1.75
-print(height)
-
---BOOLEAN
-local isAlive = true
-print(isAlive)
